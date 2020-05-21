877ee57398e642464b74195649b6ca4d7d267353
Jess Robertson <jesse.robertson@mbie.govt.nz>
2020-05-21 15:17:59 +1200

    🐛 Handle datetime bug in JSON enc

--- examples/convert.py
+++ examples/convert.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, IO, Union
+from datetime import datetime, date
 
 import click
 import yaml
@@ -10,6 +10,15 @@ except ImportError:
     from yaml import Loader
 
 
+def datetime_handler(obj):
+    "Seperate handler to handle datetimes -> JSON"
+    return (
+        obj.isoformat()
+        if isinstance(obj, (datetime, date))
+        else json.JSONEncoder().default(obj)
+    )
+
+
 @click.command()
 @click.option(
     "--indent", type=int, default=4, help="The indent side for the output JSON"
@@ -18,7 +27,8 @@ except ImportError:
 def main(source, indent):
     "Convert a YAML file to pretty JSON"
     data = yaml.load(source, Loader=Loader)
-    click.echo(json.dumps(data, indent=indent))
+    output = json.dumps(data, indent=indent, default=datetime_handler)
+    click.echo(output)
 
 
 if __name__ == "__main__":
