9856d278ae5e56e95f3bc80261dea42d3a01f564
Jess Robertson <jesse.robertson@mbie.govt.nz>
2020-05-25 09:07:55 +1200

    🧪 Add compilation tests for schemas
    Checks that schemas compile without errors

---
+++ tests/test_schemas_valid.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import json
+
+import fastjsonschema as jsonschema
+import pytest
+
+# Find all the JSON Schema files in the folder
+SCHEMA_FOLDER = Path(__file__).parent.parent / "schema.igsn.org" / "json"
+TO_CHECK = [
+    SCHEMA_FOLDER / "description" / "geoSample" / "v0.1",
+    SCHEMA_FOLDER / "registration" / "v0.1",
+]
+SCHEMAS = []
+for directory in TO_CHECK:
+    SCHEMAS.extend(directory.glob("*.schema.json"))
+
+
+def test_folders_exist():
+    for directory in TO_CHECK:
+        assert directory.exists()
+
+
+@pytest.mark.parametrize("schema", SCHEMAS)
+def test_schemas_exist(schema):
+    assert schema.exists()
+
+
+@pytest.mark.parametrize("schema", SCHEMAS)
+def test_schema_compiles(schema):
+    # Load up schema
+    assert schema.exists()
+    with open(schema, "rb") as source:
+        data = json.load(source)
+
+    # Check that we've got a valid schema
+    try:
+        jsonschema.compile(data)
+    except jsonschema.JsonSchemaDefinitionException as exc:
+        msg = f"Schema {schema} failed to compile.\n\n"
+        msg += f"Error was:\n{exc.message}"
+        raise AssertionError(msg)
