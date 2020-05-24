70dccf28955060ba539bf46d007695761000d1a3
Jess Robertson <jesse.robertson@mbie.govt.nz>
2020-05-25 08:21:32 +1200

    🧪Adding validation tests for registration data

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import json
 from datetime import date, datetime
 
+from fastjsonschema import JsonSchemaException
 import pytest
 import yaml
 
@@ -19,8 +20,31 @@ FILES = defaultdict(list)
 for fname in EXAMPLES.iterdir():
     FILES[fname.stem].append(fname.suffix)
 FILENAMES = [name for name, ext in FILES.items() if set(ext) == {".yaml", ".json"}]
-YAML_FILES = [EXAMPLES / f"{fname}.yaml" for fname in FILENAMES]
-JSON_FILES = [EXAMPLES / f"{fname}.json" for fname in FILENAMES]
+YAML_FILES = [f"{fname}.yaml" for fname in FILENAMES]
+JSON_FILES = [f"{fname}.json" for fname in FILENAMES]
+
+# Unwrap all our JSON files into seperate documents
+JSON_DOCUMENTS = {}
+for jfile in JSON_FILES:
+    name = jfile.split('.')[0]
+    with open(EXAMPLES / jfile, "rb") as source:
+        data = json.load(source)
+
+    if isinstance(data, list):
+        for idx, document in enumerate(data):
+            JSON_DOCUMENTS[f"{name}[{idx}]"] = document
+    else:
+        JSON_DOCUMENTS[name] = data
+
+
+@pytest.mark.parametrize("name,document", JSON_DOCUMENTS.items())
+def test_json_validates(registration_validator, name, document):
+    "Examples should validate against our schema"
+    # Check validation, wrap in some extra info if required
+    try:
+        registration_validator(document)
+    except JsonSchemaException as exc:
+        raise ValueError(f'Document {name}.json failed to validate.\n\nError was:\n{exc.message}')
 
 
 # Include our conversion code here
@@ -33,19 +57,11 @@ def datetime_handler(obj):
     )
 
 
-@pytest.mark.parametrize("json_file", JSON_FILES)
-def test_json_validates(json_file):
-    "Examples should validate against our schema"
-    assert json_file.exists()
-    with open(json_file, 'rb') as source:
-        data = json.load(source)
-        assert data
-
-
 @pytest.mark.parametrize("yaml_file, json_file", zip(YAML_FILES, JSON_FILES))
 def test_yaml_json_equal(yaml_file, json_file):
     "The YAML and JSON versions should be the same..."
     # Sanity check yaml data
+    yaml_file = EXAMPLES / yaml_file
     assert yaml_file.exists()
     with open(yaml_file, "r") as source:
         documents = list(yaml.load_all(source, Loader=Loader))
@@ -54,6 +70,7 @@ def test_yaml_json_equal(yaml_file, json_file):
         )
 
     # Load and serialize JSON to remove pretty-printing
+    json_file = EXAMPLES / json_file
     assert json_file.exists()
     with open(json_file, "rb") as source:
         data = json.load(source)
